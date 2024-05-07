24f8664d74a8244582c1c69d221eb87019013a70
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-05-07 22:04:44 +0300

    07.05 Doing different sizes in a method (not done yet).

--- src/main/java/com/anarut/gamefifteen/view/GameMenuButton.java
+++ src/main/java/com/anarut/gamefifteen/view/GameMenuButton.java
@@ -1,47 +1,44 @@
 package com.anarut.gamefifteen.view;
 
 import com.anarut.gamefifteen.Constants;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-import java.security.PublicKey;
-
 public class GameMenuButton extends Button {
     private Stage stage;
-    static final int LARGE_SCREEN = 24;
-    static final int MEDIUM_SCREEN = 16;
-    static final int SMALL_SCREEN = 8;
+
+//    static final int largeScreen = 24;
+//    static final int mediumScreen = 16;
+//    static final int smallScreen = 8;
+
+    private int screenSize;
+
 
     public GameMenuButton() {
-        setFont(Font.font(16));
+
+        if (screenSize <= 8) {
+            setPrefWidth((double) Constants.BUTTON_PREF_WIDTH / 2);
+            setFont(Font.font(screenSize));
+        }
+        if (screenSize >= 8 && screenSize < 20) {
+            setPrefWidth(Constants.BUTTON_PREF_WIDTH);
+            setFont(Font.font(screenSize));
+        }
+        if (screenSize >= 24) {
+            setPrefWidth(Constants.BUTTON_PREF_WIDTH * 2);
+            setFont(Font.font(screenSize));
+        } else {
+            setFont(Font.font(12));
+        }
+
     }
 
-    public GameMenuButton(int constant) {
-        setPrefWidth(Constants.BUTTON_PREF_WIDTH);
-        setFont(Font.font(constant));
-        if (constant <= 8) {
-            stage.setMinWidth(320 / 1.5);
-            stage.setMinHeight(240/ 1.5);
-            stage.setMaxWidth(640/ 1.5);
-            stage.setMaxHeight(480 / 1.5);
-        }
-        if (constant <= 16 && constant > 8) {
-            stage.setMinWidth(320);
-            stage.setMinHeight(240);
-            stage.setMaxWidth(640);
-            stage.setMaxHeight(480);
-        }
-        if (constant >= 24) {
-            stage.setMinWidth(320 * 3.5);
-            stage.setMinHeight(240 * 3.5);
-            stage.setMaxWidth(640 * 3.5);
-            stage.setMaxHeight(480 * 3.5);
-        }
+    public int getScreenSize() {
+        return screenSize;
     }
 
-
-
-
+    public void setScreenSize(int screenSize) {
+        this.screenSize = screenSize;
+    }
 }
--- src/main/java/com/anarut/gamefifteen/view/SettingsButtonsView.java
+++ src/main/java/com/anarut/gamefifteen/view/SettingsButtonsView.java
@@ -8,10 +8,12 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class SettingsButtonsView {
     private Stage stage;
+     private GameMenuButton screenSize;
 
     public SettingsButtonsView(Stage stage) {
         this.stage = stage;
@@ -30,27 +32,54 @@ public class SettingsButtonsView {
         smallButton.setText("SMALL");
         smallButton.setPrefWidth(240);
         smallButton.setOnAction(e -> {
-            new GameMenuButton(GameMenuButton.SMALL_SCREEN);
+
+            new GameMenuButton().setFont(Font.font(8));
+                stage.setMinWidth(320 / 1.5);
+                stage.setMinHeight(240/ 1.5);
+                stage.setMaxWidth(640/ 1.5);
+                stage.setMaxHeight(480 / 1.5);
+                stage.setWidth(320 / 1.5);
+                stage.setHeight(240 / 1.5);
+
+
             new SettingsButtonsView(stage).show();
         });
         vBox.getChildren().add(smallButton);
 
 
         Button mediumButton = new GameMenuButton();
-        smallButton.setText("MEDIUM");
-        smallButton.setPrefWidth(240);
-        smallButton.setOnAction(e -> {
-            new GameMenuButton(GameMenuButton.MEDIUM_SCREEN);
+        mediumButton.setText("MEDIUM");
+        mediumButton.setPrefWidth(240);
+        mediumButton.setOnAction(e -> {
+
+            new GameMenuButton().setFont(Font.font(16));
+                stage.setMinWidth(320);
+                stage.setMinHeight(240);
+                stage.setMaxWidth(640);
+                stage.setMaxHeight(480);
+            stage.setWidth(320 );
+            stage.setHeight(240);
+
             new SettingsButtonsView(stage).show();
         });
         vBox.getChildren().add(mediumButton);
 
 
         Button largeButton = new GameMenuButton();
-        smallButton.setText("LARGE");
-        smallButton.setPrefWidth(240);
-        smallButton.setOnAction(e -> {
-            new GameMenuButton(GameMenuButton.LARGE_SCREEN);
+        largeButton.setText("LARGE");
+        largeButton.setPrefWidth(240);
+        largeButton.setOnAction(e -> {
+
+            new GameMenuButton().setFont(Font.font(24));
+                stage.setMinWidth(320 * 1.5);
+                stage.setMinHeight(240 * 1.5);
+                stage.setMaxWidth(640 * 1.5);
+                stage.setMaxHeight(480 * 1.5);
+                stage.setWidth(640 * 1.5);
+                stage.setHeight(480 * 1.5);
+
+
+
             new SettingsButtonsView(stage).show();
 
         });
