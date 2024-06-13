19f927fa96d0456a76b5b67ef97227e9183f0063
Anton from PC <antonkol1993@gmail.com>
2024-06-13 17:12:41 +0300

    13.06 Doing the method move

--- src/main/java/piatnashki_new/Main.java
+++ src/main/java/piatnashki_new/Main.java
@@ -1,19 +1,34 @@
 package piatnashki_new;
 
 import piatnashki_new.model.MainMenuModel;
+import piatnashki_new.model.gameBoard.GameBoard;
+import piatnashki_new.service.GameBoardService;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
 //        MainMenuModel.Builder builder = MainMenuModel.builder();
 //        builder.setOnNewGameAction(e -> System.out.println());
 //        builder.setOnExitAction(event -> System.out.println());
 //        MainMenuModel mainMenuModel = builder.build();
 
 
-        MainMenuModel mainMenuModel = MainMenuModel.builder()
-                .withOnNewGameAction(e -> System.out.println())
-                .withOnExitAction(event -> System.out.println())
-                .build();
+//        MainMenuModel mainMenuModel = MainMenuModel.builder()
+//                .withOnNewGameAction(e -> System.out.println())
+//                .withOnExitAction(event -> System.out.println())
+//                .build();
+
+        GameBoardService gameBoardService = GameBoardService.getInstance();
+        GameBoard gameBoard = gameBoardService.newGame(5,5);
+        System.out.println(gameBoard);
+        for(;;) {
+            Scanner scanner = new Scanner(System.in);
+            int x = scanner.nextInt();
+            gameBoardService.move(gameBoard,x);
+            System.out.println(gameBoard);
+        }
 
     }
 }
--- src/main/java/piatnashki_new/service/GameBoardService.java
+++ src/main/java/piatnashki_new/service/GameBoardService.java
@@ -46,74 +46,72 @@ public class GameBoardService {
 
     public GameBoard move(GameBoard gameBoard, int numb) {
 
-        Integer temp = gameBoard.getBoard()[2][2];
-        gameBoard.getBoard()[2][2] = gameBoard.getBoard()[3][3];
-        gameBoard.getBoard()[3][3] = temp;
 
-//        for (int i = 0; i < gameBoard.getSizeHeight(); i++) {
-//            for (int j = 0; j < gameBoard.getSizeWidth(); j++) {
-//                int zeroOnJ = gameBoard.getZeroInBoard() % gameBoard.getSizeHeight();
-//                // todo if horizontal
-//                if (gameBoard.getBoard()[i][j] == numb &&
-//                        gameBoard.getZeroInBoard() / gameBoard.getSizeHeight() == i) {
-//
-//                    if (j < zeroOnJ ) {
-//                        //todo zeroToLeft
-//                        for (int c = 0; c < zeroOnJ - j ; c++) {
-//                            int temp = gameBoard.getBoard()[i][zeroOnJ - c - 1];
-//                            gameBoard.getBoard()[i][zeroOnJ - c - 1] = 0;
-//                            gameBoard.getBoard()[i][zeroOnJ - c] = temp;
-//
-//                            gameBoard.setZeroInBoard(gameBoard.getBoard()[i][zeroOnJ - c - 1]);
-//                        }
-//                        return gameBoard;
-//
-//                    } else { //todo zeroToRight
-//                        for (int c = 0; c < j - zeroOnJ; c++) {
-//                            int temp = gameBoard.getBoard()[i][zeroOnJ + c + 1];
-//                            gameBoard.getBoard()[i][zeroOnJ + c + 1] = 0;
-//                            gameBoard.getBoard()[i][zeroOnJ + c] = temp;
-//
-//                            gameBoard.setZeroInBoard(gameBoard.getBoard()[i][zeroOnJ + c + 1]);
-//                        }
-//                        return gameBoard;
-//                    }
-//
-//                }
-//                // todo if vertical??
-//                else if (gameBoard.getBoard()[i][j] == numb &&
-//                        gameBoard.getZeroInBoard() % gameBoard.getSizeWidth() == j) {
-//
-//                    int zeroString = gameBoard.getZeroInBoard();
-//                    int numberString = (i * gameBoard.getSizeHeight() + j);
-//
-//
-//                    if (numberString < zeroString) {
-//                        //todo zero to up
-//                        for (int c = 0; c < zeroString - numberString; c++) {
-//                            int temp = gameBoard.getBoard()[i - c - 1][j];
-//                            gameBoard.getBoard()[i - c - 1][j] = 0;
-//                            gameBoard.getBoard()[i - c][j] = temp;
-//
-//                            gameBoard.setZeroInBoard(gameBoard.getBoard()[i - c - 1][j]);
-//                        }
-//
-//
-//                        return gameBoard;
-//                    } else {  //todo zero to down
-//                        for (int c = 0; c < numberString - zeroString; c++) {
-//                            int temp = gameBoard.getBoard()[i + c + 1][j];
-//                            gameBoard.getBoard()[i + c + 1][j] = 0;
-//                            gameBoard.getBoard()[i + c ][j] = temp;
-//
-//                            gameBoard.setZeroInBoard(gameBoard.getBoard()[i + c + 1][j]);
-//
-//                        }
-//                        return gameBoard;
-//                    }
-//                }
-//            }
-//        }
+
+        for (int i = 0; i < gameBoard.getSizeHeight(); i++) {
+            for (int j = 0; j < gameBoard.getSizeWidth(); j++) {
+                int zeroOnJ = gameBoard.getZeroInBoard() % gameBoard.getSizeHeight();
+                // todo if horizontal
+                if (gameBoard.getBoard()[i][j] == numb &&
+                        gameBoard.getZeroInBoard() / gameBoard.getSizeHeight() == i) {
+
+                    if (j < zeroOnJ ) {
+                        //todo zeroToLeft
+                        for (int c = 0; c < zeroOnJ - j ; c++) {
+                            int temp = gameBoard.getBoard()[i][zeroOnJ - c - 1];
+                            gameBoard.getBoard()[i][zeroOnJ - c - 1] = 0;
+                            gameBoard.getBoard()[i][zeroOnJ - c] = temp;
+
+                            gameBoard.setZeroInBoard(gameBoard.getBoard()[i][zeroOnJ - c - 1]);
+                        }
+                        return gameBoard;
+
+                    } else { //todo zeroToRight
+                        for (int c = 0; c < j - zeroOnJ; c++) {
+                            int temp = gameBoard.getBoard()[i][zeroOnJ + c + 1];
+                            gameBoard.getBoard()[i][zeroOnJ + c + 1] = 0;
+                            gameBoard.getBoard()[i][zeroOnJ + c] = temp;
+
+                            gameBoard.setZeroInBoard(gameBoard.getBoard()[i][zeroOnJ + c + 1]);
+                        }
+                        return gameBoard;
+                    }
+
+                }
+                // todo if vertical??
+                else if (gameBoard.getBoard()[i][j] == numb &&
+                        gameBoard.getZeroInBoard() % gameBoard.getSizeWidth() == j) {
+
+                    int zeroString = gameBoard.getZeroInBoard();
+                    int numberString = (i * gameBoard.getSizeHeight() + j);
+
+
+                    if (numberString < zeroString) {
+                        //todo zero to up
+                        for (int c = 0; c < zeroString - numberString; c++) {
+                            int temp = gameBoard.getBoard()[i - c - 1][j];
+                            gameBoard.getBoard()[i - c - 1][j] = 0;
+                            gameBoard.getBoard()[i - c][j] = temp;
+
+                            gameBoard.setZeroInBoard(gameBoard.getBoard()[i - c - 1][j]);
+                        }
+
+
+                        return gameBoard;
+                    } else {  //todo zero to down
+                        for (int c = 0; c < numberString - zeroString; c++) {
+                            int temp = gameBoard.getBoard()[i + c + 1][j];
+                            gameBoard.getBoard()[i + c + 1][j] = 0;
+                            gameBoard.getBoard()[i + c ][j] = temp;
+
+                            gameBoard.setZeroInBoard(gameBoard.getBoard()[i + c + 1][j]);
+
+                        }
+                        return gameBoard;
+                    }
+                }
+            }
+        }
         return gameBoard;
     }
 }
